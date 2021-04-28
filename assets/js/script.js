e44a3270bbc5b67fbd3e7b8c38fa928411f72c70
crosgar <cecilia.rossi.garcia@gmail.com>
2021-04-28 16:25:32 -0700

    fixed bugs in getUvi and getForecast, added more styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 $('#search-btn').on('click', function(){
    let cityName = $('#city-name').val();
 
-   $('.card').text('');
+   $('#current-weather').html('');
 
    getWeatherInfo(cityName);
-   getForecast(cityName);
+   
 });
 
 function getWeatherInfo(cityName) {
@@ -13,37 +13,38 @@ function getWeatherInfo(cityName) {
    fetch(apiUrl)
    .then(res => res.json())
    .then(data => {
-      let mainCard = $('<div>').addClass('card');
-      let title = $('<h3>').addClass('card-title').text(data.name);
-      // let icon = data.weather.icon;
-      // title.append(icon);
+      let mainCard = $('<div>').addClass('col main-card card w-100');
+      let title = $('<h3>').addClass('card-title').text(`${data.name}`);
+      let icon = $('<img>').attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`).addClass('forecast-icon');
+      let date = $('<p>').addClass('card-text').text(`${new Date().toLocaleString().split(',')[0]}`);
       let temp = $('<p>').addClass('card-text').text(`Temp: ${data.main.temp}\xB0F`);
       let wind = $('<p>').addClass('card-text').text(`Wind Speed: ${data.wind.speed} MPH`);
       let humidity = $('<p>').addClass('card-text').text(`Humidity: ${data.main.humidity}\x25`);
       let lat = data.coord.lat;
       let lon = data.coord.lon;
 
-      //getUvi(lat, lon);
-
+      title.append(date, icon);
       mainCard.append(title, temp, wind, humidity);
 
       $('#current-weather').append(mainCard);
 
-
+      getUvi(lat, lon);
+      getForecast(cityName);
    });
 };
 
-// function getUvi(lat, lon) {
-//    let secondApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=a4fc2c49c4ea222299041dc24c1b4c99`;
+function getUvi(lat, lon) {
+   let secondApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=a4fc2c49c4ea222299041dc24c1b4c99`;
 
-//    fetch(secondApiUrl)
-//    .then(res => res.json())
-//    .then(data => {
-//       let uvIndex = $('<p>').addClass('uv-index').text(`UV Index: ${data.current.uvi}`);
-//       console.log(uvIndex);
-//    });
+   fetch(secondApiUrl)
+   .then(res => res.json())
+   .then(data => {
+      let uvIndex = $('<p>').addClass('uv-index').text(`UV Index: ${data.current.uvi}`);
+      
+      $('.main-card').append(uvIndex);
+   });
 
-//};
+};
 
 function getForecast(cityName) {
    let thirdApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=a4fc2c49c4ea222299041dc24c1b4c99&units=imperial`;
@@ -51,12 +52,19 @@ function getForecast(cityName) {
    fetch(thirdApiUrl)
    .then(res => res.json())
    .then(data => {
-      let cardOne = $('div').addClass('card col-2');
-      let title = $('<h4>').addClass('card-title').text(data.list[1].dt_txt);
-      let temp = $('<p>').addClass('card-text').text(`Temp: ${data.list[1].main.temp}\xB0F`);
+      $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\"\justify-content-center\">");
 
-      cardOne.append(title, temp);
+      for (let i = 4; i < 40; i += 8) {
+         let card = $('<div>').addClass('card col-sm-12 col-md-6 col-lg-2');
+         let title = $('<h4>').addClass('card-title').text(new Date(data.list[i].dt_txt).toLocaleString().split(',')[0]);
+         let icon = $('<img>').attr('src', `http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`).addClass('forecast-icon');
+         let temp = $('<p>').addClass('card-text').text(`Temp: ${data.list[i].main.temp}\xB0F`);
+         let wind = $('<p>').addClass('card-text').text(`Wind Speed: ${data.list[i].wind.speed} MPH`);
+         let humidity = $('<p>').addClass('card-text').text(`Humidity: ${data.list[i].main.humidity}\x25`);
 
-      $('#forecast').append(cardOne);
+         card.append(title, icon, temp, wind, humidity);
+
+         $('#forecast .row').append(card);
+      }
    });   
 };
